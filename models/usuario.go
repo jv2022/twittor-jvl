c5d8342f06defe095efcb596313fae6cb83b7aef
JOSE VELARDE <jose.antonio.velarde.luyo@gmail.com>
2022-09-22 20:29:30 -0300

    Registro de usuario v2

--- models/usuario.go
+++ models/usuario.go
@@ -13,7 +13,7 @@ Modelo de usuario de la base de datos MongoDB`
 type Usuario struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Nombre          string             `bson:"nombre" json:"nombre,omitempty"`
-	Apellidos       string             `bson:"apellidos" json:"apellido,omitempty"`
+	Apellidos       string             `bson:"apellidos" json:"apellidos,omitempty"`
 	FechaNacimiento time.Time          `bson:"fechaNacimiento" json:"fechaNacimiento,omitempty"`
 	Email           string             `bson:"email" json:"email"`
 	Password        string             `bson:"password" json:"password,omitempty"`
