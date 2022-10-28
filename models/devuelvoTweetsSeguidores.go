9829918f90d92d3ca4dde45878909c89c7f9c8e1
JOSE VELARDE <jose.antonio.velarde.luyo@gmail.com>
2022-10-28 11:28:54 -0300

    commit final

--- models/devuelvoTweetsSeguidores.go
+++ models/devuelvoTweetsSeguidores.go
@@ -12,7 +12,7 @@ DevuelvoTweetsSeguidores, es la estructura con la que devolveremos los tweets de
 type DevuelvoTweetsSeguidores struct {
 	ID                primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	UsuarioID         string             `bson:"usuarioid" json:"userId,omitempty"`
-	UsuarioRelacionID string             `bson:"usuariorealcionid" json:"userRelationId,omitempty"`
+	UsuarioRelacionID string             `bson:"usuariorelacionid" json:"userRelationId,omitempty"`
 
 	Tweet struct {
 		Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
